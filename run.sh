29e2bf851422184dbae6fcd5e03f3f629e411c14
eddy <18621781488@163.com>
2022-05-27 14:15:37 +0800

    fix

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --rm -v /tmp/pids:/app/tmp/pids -v /tmp/sockets:/app/tmp/sockets -v /tmp/production.sqlite3:/app/db/production.sqlite3 -v /Users/eddy/workspace/yuanyuan/db/production.sqlite3:/app/db/production.sqlite3 yuanyuan:release-0.1
+docker run --rm -v /tmp/pids:/app/tmp/pids -v /tmp/sockets:/app/tmp/sockets -v /tmp/production.sqlite3:/app/db/production.sqlite3 yuanyuan:release-0.1
